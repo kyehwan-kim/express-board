650f2ecc75d88732ed312e4a4184512f3ecd3dac
kyehwan <clon0306@gmail.com>
2023-03-21 19:20:14 +0900

    mongoDB: SQL -> MongoDB 코드로 변경

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,28 +1,29 @@
-const connection = require('./dbConnect');
+const mongoClient = require('./mongoConnect');
 
 const userDB = {
   // 중복회원찾기
-  userCheck: (userID, cb) => {
-    connection.query(
-      `SELECT * FROM mydb.user WHERE USERID = '${userID}';`,
-      (err, data) => {
-        if (err) throw err;
-        console.log(data);
-        cb(data);
-      },
-    );
+  userCheck: async (userID) => {
+    try {
+      const client = await mongoClient.connect();
+      const user = client.db('kdt5').collection('user');
+      const findUser = await user.findOne({ id: userID });
+      return findUser;
+    } catch (err) {
+      console.error(err);
+    }
   },
 
   // 회원 가입 하기
-  registerUser: (newUser, cb) => {
-    connection.query(
-      `INSERT INTO mydb.user (USERID, PASSWORD) VALUES ('${newUser.id}', '${newUser.password}');`,
-      (err, data) => {
-        if (err) throw err;
-        console.log(data);
-        cb(data);
-      },
-    );
+  registerUser: async (newUser) => {
+    try {
+      const client = await mongoClient.connect();
+      const user = client.db('kdt5').collection('user');
+
+      await user.insertOne(newUser);
+      return true;
+    } catch (err) {
+      console.error(err);
+    }
   },
 };
 
